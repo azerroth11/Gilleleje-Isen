847e959638932fee9b39a699caaefe3b75cdd66b
azerroth11 <mallory.berthome@gmail.com>
2021-06-28 13:02:51 +0200

    Working script

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 let strict
 
 const start = document.querySelector(".start")
-const price = document.querySelector(".price")
+const priceH1 = document.querySelector(".priceH1")
 const size = document.querySelector("#size")
+const isCard = document.querySelector(".is-card")
+const sizeCard = document.querySelector(".size-card")
+const basketCard = document.querySelector(".basket-card")
 
 start.addEventListener("click", e => {
-  const card = document.querySelector(".card")
-  const order = document.querySelector(".order")
-  card.classList.toggle("hidden")
-  order.classList.toggle("hidden")
+  isCard.classList.toggle("hidden")
+  sizeCard.classList.toggle("hidden")
 })
 
+// Display correct number of tastes options
 size.addEventListener("input", e => {
   const choice1 = document.querySelector("#choice1")
   const choice2 = document.querySelector("#choice2")
@@ -18,25 +20,117 @@ size.addEventListener("input", e => {
   const choice4 = document.querySelector("#choice4")
   const choice5 = document.querySelector("#choice5")
   if (size.value == "medium") {
-    price.innerText = "DKK 89"
+    priceH1.innerText = "DKK 89"
     choice1.classList.remove("hidden")
     choice2.classList.remove("hidden")
     choice3.classList.remove("hidden")
     choice4.classList.add("hidden")
     choice5.classList.add("hidden")
   } else if (size.value == "large") {
-    price.innerText = "DKK 152"
+    priceH1.innerText = "DKK 152"
     choice1.classList.remove("hidden")
     choice2.classList.remove("hidden")
     choice3.classList.remove("hidden")
     choice4.classList.remove("hidden")
     choice5.classList.remove("hidden")
   } else {
-    price.innerText = ""
+    priceH1.innerText = ""
     choice1.classList.add("hidden")
     choice2.classList.add("hidden")
     choice3.classList.add("hidden")
     choice4.classList.add("hidden")
     choice5.classList.add("hidden")
+  }
+})
+
+// Verify if != undefined
+document.addEventListener("click", e => {
+  const smagvariant = document.querySelectorAll(".select")
+  const select = Array.from(smagvariant)
+  select.forEach(e => {
+    if (e.value == "undefined") {
+      e.classList.add("undefined")
+    } else {
+      e.classList.remove("undefined")
+    }
+  })
+})
+
+// Tastes list generation
+const add = document.querySelector("#add")
+let itemCount = 0
+let totalPriceAmount = 0
+
+add.addEventListener("click", e => {
+  const smagvariant1 = document.querySelector("#smagsvariant1").value
+  const smagvariant2 = document.querySelector("#smagsvariant2").value
+  const smagvariant3 = document.querySelector("#smagsvariant3").value
+  const smagvariant4 = document.querySelector("#smagsvariant4").value
+  const smagvariant5 = document.querySelector("#smagsvariant5").value
+  let itemList = document.querySelector(".list")
+  const itemCountEl = document.querySelector("#itemCount")
+
+  if (size.value == "medium" && smagvariant1 !== "undefined") {
+    const list = [smagvariant1, smagvariant2, smagvariant3]
+    const mediumPrice = 89
+    itemCount++
+    itemCountEl.textContent = " " + itemCount
+    itemList.classList.add("hidden")
+    olList = document.createElement("li")
+    olList.appendChild(
+      document.createTextNode("750ml is: " + list + " (" + mediumPrice + "dkk)")
+    )
+    basketCard.insertBefore(olList, h)
+    totalPrice(mediumPrice)
+  } else if (size.value == "large" && smagvariant1 !== "undefined") {
+    const list = [
+      smagvariant1,
+      smagvariant2,
+      smagvariant3,
+      smagvariant4,
+      smagvariant5,
+    ]
+    const largePrice = 152
+    itemCount++
+    itemCountEl.textContent = " " + itemCount
+    itemList.classList.add("hidden")
+    olList = document.createElement("li")
+    olList.appendChild(
+      document.createTextNode("1500ml is: " + list + " (" + largePrice + "dkk)")
+    )
+    basketCard.insertBefore(olList, h)
+    totalPrice(largePrice)
+  } else {
+    alert("You must pick a size")
+  }
+})
+
+let h = document.createElement("H2")
+let t = document.createTextNode("")
+let btnBack = document.createElement("a")
+btnBack.setAttribute("href", "shop.html")
+btnBack.innerHTML = "Add more!"
+btnBack.classList.add("start")
+let btnForth = document.createElement("a")
+btnForth.setAttribute("href", "#")
+btnForth.innerHTML = "Proceed to checkout"
+btnForth.classList.add("start")
+basketCard.appendChild(h)
+basketCard.appendChild(btnBack)
+basketCard.appendChild(btnForth)
+
+function totalPrice(price) {
+  totalPriceAmount = totalPriceAmount + price
+  t.textContent = "Total price is: " + totalPriceAmount + "dkk"
+  h.appendChild(t)
+}
+
+const cart = document.querySelector(".shop")
+
+cart.addEventListener("click", e => {
+  isCard.classList.add("hidden")
+  sizeCard.classList.add("hidden")
+  basketCard.classList.remove("hidden")
+  if (cart.textContent != 0) {
   }
 })
